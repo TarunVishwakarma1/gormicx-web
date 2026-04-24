60c6b01db228a9ba8b9c7a41f32055a15131b5b5
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2026-04-25 03:21:12 +0530

    Fix GitHub Actions configure-pages crash

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const basePath = process.env.GITHUB_ACTIONS && process.env.GITHUB_REPOSITORY
+  ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
+  : ''
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
@@ -9,6 +13,7 @@ const nextConfig = {
   images: { unoptimized: true },
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true }, // no eslint-config-next in deps
+  basePath: basePath,
 }
 
 module.exports = withNextra(nextConfig)
