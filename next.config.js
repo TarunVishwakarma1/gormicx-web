76ce6c3b1c83abb61246807a37947ce0c09bbf39
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2026-04-25 03:27:22 +0530

    Remove basePath since site uses custom domain

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-const basePath = process.env.GITHUB_ACTIONS && process.env.GITHUB_REPOSITORY
-  ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
-  : ''
-
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
@@ -13,7 +9,6 @@ const nextConfig = {
   images: { unoptimized: true },
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true }, // no eslint-config-next in deps
-  basePath: basePath,
 }
 
 module.exports = withNextra(nextConfig)
